c82d638803f6ce462de232eaced54f54efd50876
Chandra <62059587+Chandra-1@users.noreply.github.com>
2022-02-17 00:08:18 +0700

    add components

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Navbar.module.css";
+import styled from "styled-components";
+const Navbar = () => {
+  return (
+    <Container>
+
+    </Container>
+    );
+};
+
+const Container = styled.div``;
+
+
+export default Navbar;
